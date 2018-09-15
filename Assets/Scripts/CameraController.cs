5ad7259f96706e0dba500de12829c91d1964a1dd
Joel <jswanson993@gmail.com>
2018-09-15 00:54:12 -0600

    Create CameraController and add 2 Cameras

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [Header("Camera Perspectives")]
+    public bool is2D = false;
+    public Camera camera2D;
+    public Camera camera3D;
+
+	// Use this for initialization
+	void Start () {
+        camera2D.enabled = false;
+        camera3D.enabled = true;
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+
+//	}
+
+    void switchPerspective() {
+        if (is2D) {
+            camera3D.enabled = true;
+            camera2D.enabled = false;
+            
+        } else {
+            camera2D.enabled = true;
+            camera3D.enabled = false;
+        }
+    }
+}
