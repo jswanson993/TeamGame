6ef867b2f042244b3172c936c8c6de0881aa8409
Joel <jswanson993@gmail.com>
2018-09-21 21:30:00 -0600

    Whitebox hub for pitch

---
+++ Assets/Scripts/HitTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HitTrigger : MonoBehaviour {
+
+    public UnityEvent triggerEffect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void hitTrigger() {
+        InvokeTrigger();
+    }
+
+    public void InvokeTrigger() {
+        triggerEffect.Invoke();
+    }
+}
---
+++ Assets/Scripts/LoadNextArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+
+public class LoadNextArea : MonoBehaviour {
+
+    public string scene;
+    public bool isActive = false;
+    
+
+    private void OnCollisionEnter(Collision collision) {
+        if (isActive) {
+            EditorSceneManager.LoadScene(scene);
+        }
+    }
+}
