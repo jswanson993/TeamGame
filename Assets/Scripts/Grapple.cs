537395b9de5196f6a60f635b13a834f912289d73
Joel <jswanson993@gmail.com>
2018-09-30 15:34:11 -0600

    Start 2d Grapple

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -16,7 +16,6 @@ public class Grapple : MonoBehaviour {
     private Vector3 endpoint;
     private Vector3 currentPos;
     private bool leftSurface;
-    private int count = 0;
 
     // Use this for initialization
     void Start () {
@@ -31,11 +30,20 @@ public class Grapple : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("Fire2") && hasGrapple) {
             if (!fired) {
-                grapple();
+                if (is3D) {
+                    grapple();
+                } else {
+                    grapple2D();
+                }
                 //Checks to see if the point the player is trying to grapple to is close enough.
                 if (Math.Abs(endpoint.x - currentPos.x) <= grappleDistance && Math.Abs(endpoint.y - currentPos.y) <= grappleDistance && Math.Abs(endpoint.z - currentPos.z) <= grappleDistance) {
                     fired = true;
-                    forwardPos = Camera.main.transform.forward;
+                    if (is3D) {
+                        forwardPos = Camera.main.transform.forward;
+                    } else {
+                        
+                        forwardPos = shotPoint.forward;
+                    }
                     GetComponent<Rigidbody>().useGravity = false;
                     
                 }
@@ -49,20 +57,24 @@ public class Grapple : MonoBehaviour {
             if (is3D) {  
                 //Moves the player to the point of where they grappled
                 this.transform.Translate(forwardPos * Time.deltaTime * grappleSpeed);
-                count++;
+                StartCoroutine(checkSameSurface());
                 //Checks to see if you fired at the same surface you are on
-                if (count == 3) {
-                    if (!leftSurface) {
-                        fired = false;
-                    }
-                    count = 0;
-                }
+                
                 
             } else {
-                //grapple2D();
+                this.transform.Translate(forwardPos * Time.deltaTime * grappleSpeed);
             }
         } else {
             GetComponent<Rigidbody>().useGravity = true;
+        }
+
+    }
+
+    IEnumerator checkSameSurface() {;
+        yield return new WaitForSeconds(.05f);
+        if (!leftSurface) {
+            Debug.Log("Checking");
+            fired = false;
         }
 
     }
@@ -88,7 +100,23 @@ public class Grapple : MonoBehaviour {
     }
 
     private void grapple2D() {
+        int layerMask = 1 << 11;
+        layerMask = ~layerMask;
+        Vector3 endpoint;
+        RaycastHit hit;
+        if (Physics.Raycast(shotPoint.position, shotPoint.forward, out hit, Mathf.Infinity, layerMask)) {
+            Debug.DrawRay(shotPoint.position, shotPoint.forward * hit.distance, Color.red, 1f);
+            endpoint = hit.point;
 
+        } else {
+            Debug.DrawRay(shotPoint.position, transform.TransformDirection(shotPoint.forward) * 1000, Color.white);
+            endpoint = transform.TransformDirection(shotPoint.forward) * 1000;
+            Debug.Log("RayMissed");
+        }
+
+        GetComponent<LineRenderer>().enabled = true;
+        GetComponent<LineRenderer>().SetPositions(new Vector3[] { shotPoint.position, endpoint });
+        Invoke("RemoveTrail", .06f);
     }
 
     private void OnCollisionEnter(Collision collision) {
