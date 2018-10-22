0de0415994564a48db46593ad830b6ee6d641853
Joel <jswanson993@gmail.com>
2018-10-21 22:02:15 -0600

    Add Damage Script

---
+++ Assets/Scripts/DealDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamage : MonoBehaviour {
+
+    public string takesDamage;
+    public string damageFunction = "takeDamage";
+    public int damageDealt = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(takesDamage)){
+            other.SendMessage(damageFunction, damageDealt, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        if (other.CompareTag(takesDamage)) {
+            other.SendMessage(damageFunction, damageDealt, SendMessageOptions.DontRequireReceiver);
+        }
+     
+    }
+}
