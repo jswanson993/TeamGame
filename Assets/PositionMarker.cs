25c6dfbeadd9cf7a8e04b54b6ca65faae870ea52
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2018-10-27 19:30:02 -0600

    spawn points started

---
+++ Assets/PositionMarker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionMarker : MonoBehaviour {
+
+	// Use this for initialization
+    public bool isStart;//should be only one per scene
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ScenePositionLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePositionLoader : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject[] PositionMarkers;
+    public GameObject playerPrefab;
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+        PositionMarkers = GameObject.FindGameObjectsWithTag("Respawn");
+        foreach (GameObject item in PositionMarkers)
+        {
+            if (item.GetComponent<PositionMarker>().isStart)
+            {
+                Instantiate(playerPrefab, item.transform.GetChild(0).position, Quaternion.identity).transform.GetChild(2).rotation = item.transform.GetChild(0).rotation;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
