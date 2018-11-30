33b0cb8b67f35938dc01c0061235000371716e1d
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2018-11-29 19:09:12 -0700

    Added main menu and overworld music
    pull might be a bit big

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    // Use this for initialization
+    public static MusicManager instance = null;
+    void Start () {
+
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
